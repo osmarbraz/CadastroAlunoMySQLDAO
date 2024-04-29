281658e4252f09386c083878a48a69de2a0aced0
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2024-04-29 20:46:49 -0300

    Documentação

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -5,9 +5,9 @@ import visao.FrmMenuPrincipal;
 public class Principal {
 
     public static void main(String[] args) {
-        //Instancia a interface gráfica
+        // Instancia a interface gráfica
         FrmMenuPrincipal objetotela = new FrmMenuPrincipal();
-        //Torna a janela visível
+        // Torna a janela visível
         objetotela.setVisible(true);
     }
 }
